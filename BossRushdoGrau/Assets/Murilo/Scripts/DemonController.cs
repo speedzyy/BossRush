c9665f2a1ebdeeeb1ae8be985c8c9d171daf5cf2
RimokiDS <murilorimoki.brs@gmail.com>
2023-10-31 11:50:48 -0300

    ajustes nas plataformas e código
    ajeitar o código no IF pq ta todo fudido

--- BossRushdoGrau/Assets/Murilo/Scripts/DemonController.cs
+++ BossRushdoGrau/Assets/Murilo/Scripts/DemonController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,63 @@ using UnityEngine.SceneManagement;
 
 public class DemonController : MonoBehaviour
 {
-    
+    public GameObject Player;
+    public GameObject tpA;
+    public GameObject tpB;
+    public GameObject tpC;
+    public GameObject tpD;
+    public GameObject tpE;
+    public GameObject pointA;
+    public GameObject pointB;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+    }
+
+    void Update()
+    {
+
+        Vector2 point = currentPoint.position - transform.position;
+
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 1f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 1f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+
+//ABAIXO ESTÁ O CÓDIGO PARA O BOSS VIRAR PARA A DIREÇÃO QUE O JOGADOR ESTIVER EM RELAÇÃO A ELE.            
+        Vector2 scale = transform.localScale;
+
+        if (Player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1;
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
+    }
 }
